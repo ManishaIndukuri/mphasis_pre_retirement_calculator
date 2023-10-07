1902c6c17a3e662adacc26f2bc53f0cbb9d4fad2
ManishaIndukuri <132542334+ManishaIndukuri@users.noreply.github.com>
2023-10-07 16:42:35 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -52,7 +52,7 @@ exports.config = {
     //
     capabilities: [{
         browserName: 'chrome',
-        browserVersion: '117.0.5938.149'
+        browserVersion: 'latest'
     }],
 
     //
@@ -103,8 +103,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
-    chromeDriverArgs: ['--port=9515', '--url-base=\'/\''],
+    services: [],
 
 
     // Framework you want to run your specs with.
